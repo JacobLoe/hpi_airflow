470b97202239784c23c39a3611d70da99a15bad7
JacobLoe <Jcbl423@gmail.com>
2020-09-04 09:57:40 +0200

    added all extractors as tasks

--- af.py
+++ af.py
@@ -1,37 +1,64 @@
+# airflow webserver -p 8080
+# airflow scheduler
+
 from datetime import timedelta
 
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-# Operators; we need this to operate!
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.docker_operator import DockerOperator
 from airflow.utils.dates import days_ago
 
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
+# default arguments inherited by each task
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(2),
+    'retries': 3,
+    'retry_delay': timedelta(hours=1),
 }
 
-with DAG('hpi', default_args=default_args, schedule_interval=timedelta(minutes=1), catchup=False) as dag:
+with DAG('hpi_extraction', default_args=default_args, schedule_interval=timedelta(hours=2),) as dag:
+
+
+    # task_detect_new_movie =
 
     task_shot_detection = DockerOperator(
         task_id='shotdetection',
         image='jacobloe/shot_detection:0.1',
         command='/video /data/ /video/file_mappings.tsv',
+        # the volumes mounted to the image need to be given as absolute paths
         volumes=['/home/jacob/Downloads/hpi/videos:/video:ro', '/home/jacob/Downloads/hpi_airflow/static/features_videos:/data'],
     )
 
-    t1 = BashOperator(
-        task_id='t1',
-        bash_command='echo 1111111111111111111111111111111111111111111111111111111111',
+    task_extract_images = DockerOperator(
+        task_id='image_extraction',
+        image='jacobloe/extract_images:0.1',
+        command='/video /data/ /video/file_mappings.tsv',
+        volumes=['/home/jacob/Downloads/hpi/videos:/video:ro', '/home/jacob/Downloads/hpi_airflow/static/features_videos:/data'],
     )
 
-    t2 = BashOperator(
-        task_id='t2',
-        bash_command='echo 2222222222222222222222222222222222222222222222222222222222',
+    task_extract_features = DockerOperator(
+        task_id='feature_extraction',
+        image='jacobloe/extract_features:0.1',
+        command='/data/ /video/file_mappings.tsv',
+        volumes=['/home/jacob/Downloads/hpi/videos:/video:ro', '/home/jacob/Downloads/hpi_airflow/static/features_videos:/data', '/home/.keras/:/root/.keras'],
     )
 
-    task_shot_detection >> t2 >> t1
+    task_extract_aspect_ratio = DockerOperator(
+        task_id='aspect_ratio_extraction',
+        image='jacobloe/extract_aspect_ratio:0.1',
+        command='/data/ /video/file_mappings.tsv',
+        volumes=['/home/jacob/Downloads/hpi/videos:/video:ro', '/home/jacob/Downloads/hpi_airflow/static/features_videos:/data'],
+    )
+
+    task_extract_optical_flow = DockerOperator(
+        task_id='extract_optical_flow',
+        image='jacobloe/optical_flow:0.1',
+        command='/video /data /video/file_mappings.tsv',
+        volumes=['/home/jacob/Downloads/hpi/videos:/video:ro', '/home/jacob/Downloads/hpi_airflow/static/features_videos:/data'],
+    )
+
+    # tasks will be executed from left to right, tasks will only run if the preceding task was executed succesfully
+    #
+    task_shot_detection >> task_extract_images
+    task_extract_images >> [task_extract_features, task_extract_aspect_ratio]
