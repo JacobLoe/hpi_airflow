d928944639a66f2b821ede2dc77482986a2bca32
JacobLoe <Jcbl423@gmail.com>
2020-09-25 23:27:41 +0200

    removed unnecessary airflow variables, added variables to initial task

--- airflow_graph.py
+++ airflow_graph.py
@@ -1,7 +1,6 @@
-# airflow webserver -p 8080
-# airflow scheduler
-
 from datetime import timedelta
+import os
+import glob
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -23,15 +22,38 @@ default_args = {
 
 with DAG('hpi_extraction', default_args=default_args,
          schedule_interval=timedelta(seconds=10),
-         max_active_runs=1,
-         concurrency=1,) as dag:
+         max_active_runs=1,     # prevents more than one graph from running at a time
+        ) as dag:
 
-    videoids = Variable.get('videoids')
+    def push_initial_parameters(**kwargs):
+        # gets all extractor parameters from airflow variables and and push them to xcom
+        # this function should only be called once
 
-    def push_initial_videoids(**kwargs):
-        # gets ids from airflow variable and and push them to xcom
-        # this function is only called once
+        videoids = Variable.get('videoids')
+        volumes_video_path = Variable.get('volumes_video_path')
+        volumes_features_path = Variable.get('volumes_features_path')
+        volumes_file_mappings_path = Variable.get('volumes_file_mappings_path')
+        shotdetection_sensitivity = Variable.get('shotdetection_sensitivity')
+        extractor_file_extension = Variable.get('extractor_file_extension')
+        image_extraction_trim_frames = Variable.get('image_extraction_trim_frames')
+        image_extraction_frame_width = Variable.get('image_extraction_frame_width')
+        optical_flow_frame_width = Variable.get('optical_flow_frame_width')
+        optical_flow_step_size = Variable.get('optical_flow_step_size')
+        optical_flow_window_size = Variable.get('optical_flow_window_size')
+        optical_flow_top_percentile = Variable.get('optical_flow_top_percentile')
+
         kwargs['ti'].xcom_push(key='videoids', value=videoids)
+        kwargs['ti'].xcom_push(key='volumes_video_path', value=volumes_video_path)
+        kwargs['ti'].xcom_push(key='volumes_features_path', value=volumes_features_path)
+        kwargs['ti'].xcom_push(key='volumes_file_mappings_path', value=volumes_file_mappings_path)
+        kwargs['ti'].xcom_push(key='shotdetection_sensitivity', value=shotdetection_sensitivity)
+        kwargs['ti'].xcom_push(key='extractor_file_extension', value=extractor_file_extension)
+        kwargs['ti'].xcom_push(key='image_extraction_trim_frames', value=image_extraction_trim_frames)
+        kwargs['ti'].xcom_push(key='image_extraction_frame_width', value=image_extraction_frame_width)
+        kwargs['ti'].xcom_push(key='optical_flow_frame_width', value=optical_flow_frame_width)
+        kwargs['ti'].xcom_push(key='optical_flow_step_size', value=optical_flow_step_size)
+        kwargs['ti'].xcom_push(key='optical_flow_window_size', value=optical_flow_window_size)
+        kwargs['ti'].xcom_push(key='optical_flow_top_percentile', value=optical_flow_top_percentile)
 
     def process_videoids(**kwargs):
         # gets ids from another task
@@ -39,6 +61,7 @@ with DAG('hpi_extraction', default_args=default_args,
         ids = ti.xcom_pull(key='videoids')
         print('ids: ', ids)
         # run docker images with pulled ids
+        #
         # instead for testing assume one video was not processed correctly and push the other ids
         new_ids = ids[:-2]
         print('new_ids: ', new_ids)
@@ -47,6 +70,15 @@ with DAG('hpi_extraction', default_args=default_args,
 
     def check_extractor_progress(**kwargs):
         # check folders for done_files
+        # volumes_features_path = kwargs['ti'].xcom_pull(key='volumes_features_path')
+        # last_extractor = kwargs['ti'].xcom_pull(key='last_extractor')   # get the name of the last run extractor to search for
+        # features_path = os.path.join(os.path.split(volumes_features_path),**,last_extractor)
+        # pp = glob.glob(features_path)
+        # ids = ' '
+        # for p in pp:
+        #     if os.isfile(os.path.join(p, ./done)):
+        #          id = os.path.split(os.path.split(p)[0])[1]
+        #           ids.join(id)
         # for testing pull ids from xcom and push them unchanged
         ti = kwargs['ti']
         ids = ti.xcom_pull(key='videoids')
@@ -54,54 +86,16 @@ with DAG('hpi_extraction', default_args=default_args,
         # push ids of movies with the files
         kwargs['ti'].xcom_push(key='videoids', value=ids)
 
-    t1 = PythonOperator(
-        task_id='push_initial_video_ids',
-        python_callable=push_initial_videoids,
+    t1 = BashOperator(
+        task_id='t1',
+        bash_command='sleep 30'
     )
 
-    t2 = PythonOperator(
-        task_id='process_videoids',
-        python_callable=process_videoids,
+    t2 = BashOperator(
+        task_id='t2',
+        bash_command='sleep 30'
     )
-
-    t3 = PythonOperator(
-        task_id='check_extractor_progress',
-        python_callable=check_extractor_progress,
-    )
-
-    t4 = PythonOperator(
-        task_id='t4',
-        python_callable=process_videoids,
-    )
-
-    t5 = PythonOperator(
-        task_id='t5',
-        python_callable=check_extractor_progress,
-    )
-
-    t1 >> t2 >> t3 >> t4 >> t5
-    # # the volumes mounted to the image need to be given as absolute paths
-    # volumes_video_path = Variable.get('volumes_video_path')
-    # # volumes_features_path = Variable.get('volumes_features_path')
-    # # volumes_file_mappings_path = Variable.get('volumes_file_mappings_path')
-    # #
-    # # # docker parameters
-    # # shotdetection_sensitivity = Variable.get('shotdetection_sensitivity')
-    # #
-    # # image_extraction_trim_frames = Variable.get('image_extraction_trim_frames')
-    # # image_extraction_frame_width = Variable.get('image_extraction_frame_width')
-    # # # FIXME maybe use the same file_extension variable for all extractors to prevent errors
-    # # image_extraction_file_extension = Variable.get('image_extraction_file_extension')
-    # #
-    # # feature_extraction_file_extension = Variable.get('feature_extraction_file_extension')
-    # #
-    # # aspect_ratio_extraction_file_extension = Variable.get('aspect_ratio_extraction_file_extension')
-    # #
-    # # optical_flow_frame_width = Variable.get('optical_flow_frame_width')
-    # # optical_flow_step_size = Variable.get('optical_flow_step_size')
-    # # optical_flow_window_size = Variable.get('optical_flow_window_size')
-    # # optical_flow_top_percentile = Variable.get('optical_flow_top_percentile')
-    #
+    t1 >> t2
 
     # task_shotdetection = DockerOperator(
     #     task_id='shotdetection',
