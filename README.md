20d648240d1e3eb472a8b31d654643f5e6a1bf26
JacobLoe <Jcbl423@gmail.com>
2020-10-11 19:21:33 +0200

    added remaining extractors to graph, resolved issues with xcom

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ https://airflow.apache.org/docs/stable/installation.html
 3: go to the new folder and open airflow.cfg
 
     change "dags_folder" to the directoy of the repository. The path has to be absolute
-    change "load_examples" to False (not strictly necessary, but removes clutter)
+    change "load_examples" to False (not strictly necessary, but removes clutter in the ui)
     
-4: execute "airflow initdb" in terminal to add the DAG in the repository to airflow
+4: execute "airflow webserver -p 8080" in the terminal, then "http://0.0.0.0:8080/" in the browser to track the progess of the DAG visually
 
-5: execute "airflow webserver -p 8080" in the terminal, then "http://0.0.0.0:8080/" in the browser to track the progess of the DAG visually
+5: execute "airflow scheduler" in the terminal
 
-6: execute "airflow scheduler" in the terminal to start the DAG
+6: on the webserver under "http://0.0.0.0:8080/variable/list/" import the "variables.json"
+
+7: to start the graph use the button "Trigger DAG" on the home screen
+
+8: use "Graph View" to view progress of the graph during runtime
--- airflow_graph.py
+++ airflow_graph.py
@@ -1,14 +1,9 @@
 from datetime import timedelta
-import os
-import glob
 
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-# from airflow.operators.docker_operator import DockerOperator
 from docker_operator import DockerOperator
 from airflow.utils.dates import days_ago
-from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.models import Variable
 
 # default arguments inherited by each task
@@ -35,8 +30,8 @@ with DAG('hpi_extraction', default_args=default_args,
         shotdetection_sensitivity = Variable.get('shotdetection_sensitivity')
         extractor_file_extension = Variable.get('extractor_file_extension')
         image_extraction_trim_frames = Variable.get('image_extraction_trim_frames')
-        image_extraction_frame_width = Variable.get('image_extraction_frame_width')
-        optical_flow_frame_width = Variable.get('optical_flow_frame_width')
+        image_extraction_frame_width = int(Variable.get('image_extraction_frame_width'))
+        optical_flow_frame_width = int(Variable.get('optical_flow_frame_width'))
         optical_flow_step_size = Variable.get('optical_flow_step_size')
         optical_flow_window_size = Variable.get('optical_flow_window_size')
         optical_flow_top_percentile = Variable.get('optical_flow_top_percentile')
@@ -53,32 +48,6 @@ with DAG('hpi_extraction', default_args=default_args,
         kwargs['ti'].xcom_push(key='optical_flow_window_size', value=optical_flow_window_size)
         kwargs['ti'].xcom_push(key='optical_flow_top_percentile', value=optical_flow_top_percentile)
 
-
-    def check_extractor_progress(**kwargs):
-        # checks the feature folders for .done-files of the preceding extractor
-        # and returns the ids of the videos that have run successfully
-
-        # check folders for done_files
-        volumes_features_path = kwargs['ti'].xcom_pull(key='volumes_features_path')
-
-        # last_extractor should be dynamic instead of hardcoded
-        # last_extractor = kwargs['ti'].xcom_pull(key='last_extractor')   # get the name of the last run extractor to search for
-        last_extractor = 'shotdetection'
-
-        # search for all the extractor folders in the features path
-        features_path = os.path.join(os.path.split(volumes_features_path)[0][:-1], '**', last_extractor)
-        all_features = glob.glob(features_path, recursive=True)
-
-        # get the ids of the videos with .done-files
-        ids = []
-        for f in all_features:
-            if os.path.isfile(os.path.join(f, '.done')):
-                ids.append(os.path.split(os.path.split(f)[0])[1])
-        videoids = ' '.join([i for i in ids])
-
-        # push ids of videos to xcom
-        kwargs['ti'].xcom_push(key='videoids', value=videoids)
-
     get_parameters = PythonOperator(
         task_id='get_parameters',
         python_callable=push_initial_parameters,
@@ -91,32 +60,56 @@ with DAG('hpi_extraction', default_args=default_args,
     of = []
     videoids = list(Variable.get('videoids').replace(' ', ''))  # remove the spaces from the videoid list
     for i, vid in enumerate(videoids):
-        # create a chain of extractor task for each videoid idividually
+        # create a chain of extractor task for each videoid individually
+        # docker commands have to be formatted without '.format()' as it clashes with the jinja templates for xcom
         sd.append(DockerOperator(
             task_id='shotdetection_task_{t}'.format(t=str(i)),
-            image='jacobloe/shot_detection:0.2',
-            command='/video /data/ /file_mappings.tsv {id} --sensitivity {{ti.xcom_pull(key="shotdetection_sensitivity")}}'.format(id=vid),
+            image='jacobloe/shot_detection:0.1',
+            command='/video /data/ /file_mappings.tsv '+vid +
+                    ' --sensitivity {{ti.xcom_pull(key="shotdetection_sensitivity")}}',
             volumes=['{{ti.xcom_pull(key="volumes_video_path")}}', '{{ti.xcom_pull(key="volumes_features_path")}}', '{{ti.xcom_pull(key="volumes_file_mappings_path")}}'],
             xcom_all=True,
         ))
-        ei.append(BashOperator(
-            task_id='ei{}'.format(i),
-            bash_command='echo {}'.format(vid),
+        ei.append(DockerOperator(
+            task_id='image_extraction_{t}'.format(t=str(i)),
+            image='jacobloe/extract_images:0.2',
+            command='/video/ /data/ /file_mappings.tsv '+vid +
+                    ' --trim_frames {{ti.xcom_pull(key="image_extraction_trim_frames")}} '
+                    '--frame_width {{ti.xcom_pull(key="image_extraction_frame_width")}} '
+                    '--file_extension {{ti.xcom_pull(key="extractor_file_extension")}}',
+            volumes=['{{ti.xcom_pull(key="volumes_video_path")}}', '{{ti.xcom_pull(key="volumes_features_path")}}', '{{ti.xcom_pull(key="volumes_file_mappings_path")}}'],
+            xcom_all=True,
         ))
-        ef.append(BashOperator(
-            task_id='ef{}'.format(i),
-            bash_command='echo {}'.format(vid),
+        ef.append(DockerOperator(
+            task_id='feature_extraction_{t}'.format(t=str(i)),
+            image='jacobloe/extract_features:0.2',
+            command='/video/ /data/ /file_mappings.tsv '+vid +
+                    ' --file_extension {{ti.xcom_pull(key="extractor_file_extension"}}',
+            volumes=['{{ti.xcom_pull(key="volumes_video_path")}}', '{{ti.xcom_pull(key="volumes_features_path")}}',
+                     '{{ti.xcom_pull(key="volumes_file_mappings_path")}}', '/home/.keras/:/root/.keras'],
+            xcom_all=True,
         ))
-        eas.append(BashOperator(
-            task_id='eas{}'.format(i),
-            bash_command='echo {}'.format(vid),
+        eas.append(DockerOperator(
+            task_id='aspect_ratio_extraction_{t}'.format(t=str(i)),
+            image='jacobloe/extract_aspect_ratio:0.2',
+            command='/video/ /data/ /file_mappings.tsv '+vid +
+                    ' --file_extension {{ti.xcom_pull(key="extractor_file_extension"}}',
+            volumes=['{{ti.xcom_pull(key="volumes_video_path")}}', '{{ti.xcom_pull(key="volumes_features_path")}}', '{{ti.xcom_pull(key="volumes_file_mappings_path")}}'],
+            xcom_all=True,
         ))
-        of.append(BashOperator(
-            task_id='of{}'.format(i),
-            bash_command='echo {}'.format(vid),
+        of.append(DockerOperator(
+            task_id='extract_optical_flow_{t}'.format(t=str(i)),
+            image='jacobloe/optical_flow:0.2',
+            command='/video/ /data/ /file_mappings.tsv '+vid +
+                    '--frame_width {{ti.xcom_pull(key="optical_flow_frame_width"}} '
+                    '--step_size {{ti.xcom_pull(key="optical_flow_step_size"}} '
+                    '--window_size {{ti.xcom_pull(key="optical_flow_window_size"}} '
+                    '--top_percentile {{ti.xcom_pull(key="optical_flow_top_percentile"}}',
+            volumes=['{{ti.xcom_pull(key="volumes_video_path")}}', '{{ti.xcom_pull(key="volumes_features_path")}}', '{{ti.xcom_pull(key="volumes_file_mappings_path")}}'],
+            xcom_all=True,
         ))
 
-        # tasks for each id are chained together sequentially
+        # tasks for each id are chained together sequentially, image extraction is dependent ond shotdetection, feature extraction on imageextraction ...
         sd[i] >> ei[i] >> ef[i] >> eas[i] >> of[i]
 
     # all task chains are dependent on a initial task
